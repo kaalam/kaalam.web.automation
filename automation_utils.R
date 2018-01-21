5ccd262e809c154947d3f1b446c7338e4f933bbe
kaalam <kaalam@dybot.com>
2018-01-21 17:13:11 +0100

    "Only if necessary" logic implemented for upload and build doxy

--- automation_utils.R
+++ automation_utils.R
@@ -1,8 +1,34 @@
 #> Functions used by all the scripts in kaalam.web.automation
 
 
-DOXY_FOLDER_OUTPUT   <- '../kaalam.github.io/development/Jazz'
-DOXY_FOLDER_DOXYFILE <- './doxygen'
+BLOG_FOLDERS <- list(input		= '../document.source/blog',
+					 output		= '../kaalam.github.io/blog',
+					 jekyllpath = './jekyll_so-simple',
+					 web_source	= '_blog_')
+
+DOC_FOLDERS	 <- list(input		= '../document.source/documentation',
+					 output		= '../kaalam.github.io/documentation',
+					 jekyllpath	= './jekyll_documentation',
+					 web_source	= '_doc_')
+
+DOXY_FOLDERS <- list(input		= '../jazz-server/src/',
+					 output		= '../kaalam.github.io/development/Jazz',
+					 doxypath	= './doxygen',
+					 web_source	= '_doxy_')
+
+KAAL_FOLDERS <- list(input		= '../document.source/kaalam',
+					 output		= '../kaalam.github.io',
+					 jekyllpath	= './jekyll_forty',
+					 web_source	= '_kaal_')
+
+NEWS_FOLDERS <- list(input		= '../document.source/news',
+					 output		= '../kaalam.github.io/news',
+					 jekyllpath	= './jekyll_evento',
+					 web_source	= '_news_')
+
+STAT_FOLDERS <- list(input		= './jazz_01x',
+					 output		= '../kaalam.github.io/development',
+					 web_source	= '_stat_')
 
 
 library(rjazz)
@@ -12,6 +38,7 @@ jazz_host <- 'http://localhost'
 jazz_port <- ':8888'
 
 
+#>> Jazz 0.1.x uploading
 upload_file <- function(fn, web_source, url_prefix = web_source)
 {
 	obj <- readBin(fn, 'raw', n = file.size(fn))
@@ -30,8 +57,6 @@ upload_file <- function(fn, web_source, url_prefix = web_source)
 	url	  <- gsub('^\\.', paste0('/', url_prefix), fn)
 	block <- create_web_resource(web_source, url, type, obj)
 
-	cat(url)
-
 	if (grepl('/index\\.html$', url))
 	{
 		host   <- rjazz:::.host.
@@ -39,8 +64,6 @@ upload_file <- function(fn, web_source, url_prefix = web_source)
 
 		url	   <- gsub('index\\.html$', '', url)
 		upload <- charToRaw(url)
-
-		cat(' +', url)
 
 		if (RCurl::curlPerform(url			 = paste0(host, '//www.', block,'.assign_url/', web_source),
 							   infilesize	 = length(upload),
@@ -51,38 +74,123 @@ upload_file <- function(fn, web_source, url_prefix = web_source)
 
 		if (downlo$value() != '0') stop(paste('PUT function .assign_url failed :', upload))
 	}
-
-	cat('\n')
 }
 
 
-upload_site <- function(working_dir, web_source, url_prefix = web_source)
+#>> "Only if necessary" logic.
+most_recent_in_path <- function(path)
 {
-	prev_wd <- setwd(paste0(working_dir, '/_site'))
+	fn <- list.files(path = normalizePath(path), all.files = TRUE, full.names = TRUE, recursive = TRUE)
 
-	for (fn in list.files('.', recursive = T, full.names = T)) upload_file(fn, web_source, url_prefix)
+	max(file.info(fn)$mtime)
+}
+
+most_recent_uploaded <- function(web_source)
+{
+	src <- '_upl_dates_'
+	if (!(src %in% list_sources())) return (as.POSIXlt('1900-01-01'))
+
+	err <- try(date <- get_R_block(src, web_source), silent = TRUE)
+
+	if (class(err) == 'try-error') return (as.POSIXlt('1900-01-01'))
+
+	strptime(date, format = '%Y-%m-%d %H:%M:%S')
+}
+
+touch <- function(web_source)
+{
+	src <- '_upl_dates_'
+	if (!(src %in% list_sources())) create_source(src)
+
+	put_R_block(source = src, block_key = web_source, sexp = format(Sys.time(), format = '%Y-%m-%d %H:%M:%S'))
+}
+
+nothing_to_build <- function(folders)
+{
+	srct <- most_recent_in_path(folders$input)
+	dest <- most_recent_in_path(folders$output)
+
+	ret <- dest > srct
+
+	if (ret) cat('	(nothing to build)\n')
+
+	ret
+}
+
+nothing_to_upload <- function(folders)
+{
+	srct <- most_recent_in_path (folders$output)
+	dest <- most_recent_uploaded(folders$web_source)
+
+	ret <- dest > srct
+
+	if (ret) cat('	(nothing to upload)\n')
+
+	ret
+}
+
+
+#>> Upload function logic.
+upload <- function(folders, force = FALSE)
+{
+	cat('Uploading:', folders$output)
+
+	if (!force & nothing_to_upload(folders)) return(invisible())
+
+	cat(' ...')
+
+	prev_wd <- setwd(normalizePath(folders$output))
+
+	rex <- '^.*/kaalam\\.github\\.io/([[:alnum:]/_-]+)$'
+
+	if (!grepl(rex, folders$output)) stop('Unexpected format in folders$output.')
+
+	for (fn in list.files('.', recursive = T, full.names = T)) upload_file(fn, folders$web_source, gsub(rex, '\\1', folders$output))
 
 	setwd(prev_wd)
+
+	touch(folders$web_source)
+
+	cat(' done.\n')
 }
 
 
-upload_folder <- function(working_dir, web_source, url_prefix = web_source)
+#>> Build using doxygen.
+build_doygen <- function(folders, force = FALSE)
 {
-	prev_wd <- setwd(working_dir)
+	cat('Building:', folders$output)
 
-	for (fn in list.files('.', recursive = T, full.names = T)) upload_file(fn, web_source, url_prefix)
+	if (!force & nothing_to_build(folders)) return(invisible())
 
-	setwd(prev_wd)
-}
+	cat(' ...')
 
-
-build_doygen <- function(working_dir)
-{
-	prev_wd <- setwd(working_dir)
+	prev_wd <- setwd(normalizePath(folders$doxypath))
 
 	system('doxygen')
 
 	setwd(prev_wd)
+
+	cat(' done.\n')
 }
+
+
+#>> Build using jekyll.
+build_jekyll <- function(folders, force = FALSE)
+{
+	cat('Building:', folders$output)
+
+	if (!force & nothing_to_build(folders)) return(invisible())
+
+	cat(' ...')
+
+	prev_wd <- setwd(normalizePath(folders$doxypath))
+
+	system('doxygen')
+
+	setwd(prev_wd)
+
+	cat(' done.\n')
+}
+
 
 set_jazz_host(paste0(jazz_host, jazz_port))

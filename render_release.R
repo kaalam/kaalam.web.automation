1a78b3edb522ef70bd8f9784b6eeb0b8c565f238
kaalam <kaalam@dybot.com>
2018-01-21 21:13:47 +0100

    Redirect added to the root address

--- render_release.R
+++ render_release.R
@@ -4,6 +4,9 @@ source('automation_utils.R')
 
 build_all()
 
+writeLines('<html><head><meta http-equiv="refresh" content="1; url=kaalam.github.io/kaalam/index.html"></head></html>',
+		   '../kaalam.github.io/index.html')
+
 setwd('../kaalam.github.io/')
 
 system('./auto_add_commit_push.sh')

4017923018d811b92982d819de69bf8ed94939a5
kaalam <kaalam@dybot.com>
2018-02-03 22:07:11 +0100

    New scaffolding.

--- audit.R
+++ audit.R
@@ -1,6 +1,9 @@
 #!/usr/bin/Rscript
 
 
+source('automation_utils.R')
+
+
 audit_http <- function()
 {
 	txt <- system('grep -r jekyll* -e http', intern = TRUE)
@@ -31,12 +34,50 @@ audit_http <- function()
 
 }
 
+
 audit_known_js <- function()
 {
 
 }
 
+
 audit_file_types <- function()
 {
 
 }
+
+
+audit_file <- function(fn, web_source)
+{
+	rex <- '^(.*/)([[:alnum:]_.-]+)\\.([[:alnum:]]+)$'
+
+	if (!grepl(rex, fn)) stop(paste('Unexpected file format', fn))
+
+}
+
+
+audit <- function(folders)
+{
+	cat('Learning:', folders$input, '...')
+
+
+
+	cat(' done.\n')
+
+	cat('Auditing:', folders$output, '...')
+
+	prev_wd <- setwd(normalizePath(folders$output))
+
+	rex <- '^.*/kaalam\\.github\\.io/([[:alnum:]/_-]+)$'
+
+	if (!grepl(rex, folders$output)) stop('Unexpected format in folders$output.')
+
+	for (fn in list.files('.', recursive = T, full.names = T)) audit_file(fn, folders$web_source)
+
+	setwd(prev_wd)
+
+	cat(' done.\n')
+}
+
+
+invisible(sapply(ALL_FOLDERS, audit))

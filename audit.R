9613003d57742053db0ebb51bff5c12347ef1549
kaalam <kaalam@dybot.com>
2018-02-05 18:33:12 +0100

    PoC version of audit.R complete.

--- audit.R
+++ audit.R
@@ -14,75 +14,90 @@ KNOWN_EXTENSIONS   <- c(BITMAP_EXTENSIONS, STATIC_EXTENSIONS, FONTS_EXTENSIONS, 
 PATH_KNOWN_BITMAPS <- '~/kaalam.etc/web_security/known_bitmaps'
 PATH_KNOWN_FONTS   <- '~/kaalam.etc/web_security/known_fonts'
 PATH_KNOWN_JS	   <- '~/kaalam.etc/web_security/known_js'
-
-hashed_folder	<- function(hash, name, type, found) data.frame(hash = hash, name = name, type = type, found = found, stringsAsFactors = FALSE)
-warn_event		<- function(level, source, issue) data.frame(level = level, source = source, issue = issue, stringsAsFactors = FALSE)
-stat_even		<- function(type, files, bytes, last) data.frame(type = type, files = files, bytes = bytes, last = last, stringsAsFactors = FALSE)
-
-DF_BITMAPS		<- hashed_folder('', '', '', '')[-1, ]
-DF_FONTS		<- hashed_folder('', '', '', '')[-1, ]
-DF_JS			<- hashed_folder('', '', '', '')[-1, ]
-DF_WARNINGS		<- warn_event('', '', '')[-1, ]
-DF_STATS		<- stat_even('', '', '', '')[-1, ]
+PATH_KNOWN_URL	   <- '~/kaalam.etc/web_security/known_urls.txt'
+PATH_KNOWN_DOMAINS <- '~/kaalam.etc/web_security/known_domains.txt'
+PATH_BLACKLISTED   <- '~/kaalam.etc/web_security/blacklisted_words.txt'
 
 REX_PAT_NAM_EXT <- '^(.*/)([[:alnum:]_.-]+)\\.([[:alnum:]]+)$'
+
+hashed_folder <- function(hash, name, type, found) data.frame(hash = hash, name = name, type = type, found = found, stringsAsFactors = FALSE)
+warn_event	  <- function(level, source, issue) data.frame(level = level, source = source, issue = issue, stringsAsFactors = FALSE)
+stat_event	  <- function(type, files, bytes, last) data.frame(type = type, files = files, bytes = bytes, last = last, stringsAsFactors = FALSE)
+
+GLOBAL <- new.env()
+
+
+load_globals <- function()
+{
+	hash_folder <- function(pat)
+	{
+		fn <- list.files(path = pat, full.names = TRUE, recursive = TRUE)
+
+		if (length(fn) == 0) return(hashed_folder('', '', '', '')[-1, ])
+
+		if (!all(grepl(REX_PAT_NAM_EXT, fn))) stop(paste('Unexpected file format in', pat))
+
+		hashfile <- function(fn) digest::digest(fn, file = TRUE)
+
+		df <- hashed_folder(hash  = sapply(fn, hashfile),
+							name  = gsub(REX_PAT_NAM_EXT, '\\2', fn),
+							type  = gsub(REX_PAT_NAM_EXT, '\\3', fn),
+							found = '')
+
+		rownames(df) <- NULL
+
+		if (any(duplicated(df$hash))) stop(paste('Duplicated resources in', pat))
+
+		df
+	}
+
+	GLOBAL$bitmaps <- hash_folder(PATH_KNOWN_BITMAPS)
+	GLOBAL$jscript <- hash_folder(PATH_KNOWN_JS)
+	GLOBAL$fonts   <- hash_folder(PATH_KNOWN_FONTS)
+
+	load_text <- function(fn) sort(unique(gsub('[[:blank:]]', '', readLines(fn))))
+
+	GLOBAL$known_urls	 <- load_text(PATH_KNOWN_URL)
+	GLOBAL$known_domains <- load_text(PATH_KNOWN_DOMAINS)
+	GLOBAL$blacklisted	 <- load_text(PATH_BLACKLISTED)
+
+	GLOBAL$warnings <- warn_event('', '', '')[-1, ]
+	GLOBAL$stats	<- stat_event('', '', '', '')[-1, ]
+}
+
+
+nice <- function(s, max_len = 35)
+{
+	if (nchar(s) <= max_len) return(s)
+
+	paste0('(..) ', substr(s, nchar(s) - max_len + 6, nchar(s)))
+}
 
 
 valid_url <- function(url)
 {
-	stop('Not implemented.')
-
-	TRUE
+	url %in% GLOBAL$known_urls
 }
 
 
 valid_http <- function(http)
 {
-	stop('Not implemented.')
+	rex <- '^(.+)/(.*)$'
 
-	TRUE
+	if (grepl(rex, http)) http <- gsub(rex, '\\1', http)
+
+	http %in% GLOBAL$known_domains
 }
 
 
-hash_folder <- function(pat)
+warning <- function(level, source, issue)
 {
-	fn <- list.files(path = pat, full.names = TRUE, recursive = TRUE)
-
-	if (length(fn) == 0) return(hashed_folder('', '', '', '')[-1, ])
-
-	if (!all(grepl(REX_PAT_NAM_EXT, fn))) stop(paste('Unexpected file format in', pat))
-
-	hashfile <- function(fn) digest::digest(fn, file = TRUE)
-
-	df <- hashed_folder(hash  = sapply(fn, hashfile),
-						name  = gsub(REX_PAT_NAM_EXT, '\\2', fn),
-						type  = gsub(REX_PAT_NAM_EXT, '\\3', fn),
-						found = '')
-
-	rownames(df) <- NULL
-
-	if (any(duplicated(df$hash))) stop(paste('Duplicated resources in', pat))
-
-	df
+	GLOBAL$warnings <- rbind(GLOBAL$warnings, warn_event(level, source, issue))
 }
 
 
-extract_capture <- function(txt, capture, fn)
+extract_capture <- function(txt, capture, fn, web_source)
 {
-	# txt <- c('aaa bbb',
-	#		 'aaa url("http://google.com")',
-	#		 'aaa url("http://google.com") bbb url("http://ibm.com") ccc',
-	#		 'url("http://google.com") bbb',
-	#		 'url("http://google.com") bbb url("http://ibm.com") ccc url("http://moft.com")',
-	#		 'url("http://google.com");url("http://ibm.com");url("http://moft.com")',
-	#		 'bbb',
-	#		 'url("http://google.com");url("http://ibm.com");url("http://moft.com")',
-	#		 ';url("http://moft.com")',
-	#		 'url("http://google.com")url("http://ibm.com");url("http://moft.com")zzz',
-	#		 'url("http://google.com")')
-	# capture <- extract_url
-	# cat(paste(txt, collapse = '\n'))
-
 	ix <- which(grepl(capture$signal, txt))
 
 	if (length(ix) == 0) return(character(0))
@@ -96,9 +111,17 @@ extract_capture <- function(txt, capture, fn)
 	if (!all(grepl(rex2, txt))) stop ('internal 1')
 	if ( any(grepl(rex3, txt))) stop ('internal 2')
 
-	if (!all(grepl(capture$capture, txt))) stop(paste('Unexpected', capture$neat, 'syntax in', fn))
+	if (all(grepl(capture$capture, txt))) return(sort(unique(gsub("'", '', gsub('"', '', gsub(capture$capture, '\\1', txt))))))
 
-	sort(unique(gsub("'", '', gsub('"', '', gsub(capture$capture, '\\1', txt)))))
+	ix <- which(!grepl(capture$capture, txt))
+
+	for (tx in txt[ix]) warning(level = 'WARN', source = web_source, issue = paste('Cannot capture in', nice(tx), 'in', nice(fn)))
+
+	ix <- which(grepl(capture$capture, txt))
+
+	if (length(ix) == 0) return(character(0))
+
+	sort(unique(gsub("'", '', gsub('"', '', gsub(capture$capture, '\\1', txt[ix])))))
 }
 
 
@@ -109,135 +132,90 @@ audit_css <- function(fn, web_source)
 
 	txt <- readLines(fn, warn = FALSE)
 
-	urls <- extract_capture(txt, extract_url, fn)
+	urls <- extract_capture(txt, extract_url, fn, web_source)
 
 	for (url in urls) {
-		if (!valid_url(url)) DF_WARNINGS <<- rbind(DF_WARNINGS,
-												   warn_event(level = 'WARN', source = web_source, issue = paste('Invalid url', url, 'in', fn)))
+		if (!valid_url(url)) warning(level = 'WARN', source = web_source, issue = paste('Invalid url', nice(url), 'in', nice(fn)))
 	}
 
-	https <- extract_capture(txt, extract_http, fn)
+	https <- extract_capture(txt, extract_http, fn, web_source)
 
 	for (http in https) {
-		if (!valid_http(http)) DF_WARNINGS <<- rbind(DF_WARNINGS,
-													 warn_event(level = 'WARN', source = web_source, issue = paste('Invalid http', http, 'in', fn)))
+		if (!valid_http(http)) warning(level = 'WARN', source = web_source, issue = paste('Invalid domain in', nice(http), 'in', nice(fn)))
 	}
 }
 
 
 audit_html <- function(fn, web_source)
 {
-	stop('Not implemented.')
+	extract_http <- list(signal = '\\<https?://', neat = 'http://', capture = '^.*\\<https?://([[:alnum:]_.-]+).*$')
+
+	txt <- readLines(fn, warn = FALSE)
+
+	https <- extract_capture(txt, extract_http, fn, web_source)
+
+	for (http in https) {
+		if (!valid_http(http)) warning(level = 'WARN', source = web_source, issue = paste('Invalid domain in', nice(http), 'in', nice(fn)))
+	}
+
+	txt <- tolower(txt)
+	txt <- gsub('[^[:alnum:]]', ' ', txt)
+	txt <- paste(txt, collapse = ' ')
+	txt <- strsplit(txt, split = ' ')[[1]]
+	txt <- sort(unique(txt))
+	txt <- txt[txt != '']
+
+	ix <- which(txt %in% GLOBAL$blacklisted)
+
+	for (i in ix) warning(level = 'WARN', source = web_source, issue = paste('Blacklisted word', nice(txt[i]), 'in', nice(fn)))
 }
 
 
 audit_bitmap <- function(fn, web_source)
 {
-	# system(paste('cp', fn, PATH_KNOWN_BITMAPS))
-	#
-	# return(T)
-
+	fn	 <- normalizePath(fn)
 	hash <- digest::digest(fn, file = TRUE)
 
-	if (nrow(DF_BITMAPS) == 0) DF_BITMAPS <<- hash_folder(PATH_KNOWN_BITMAPS)
+	ix <- which(GLOBAL$bitmaps$hash == hash)
 
-	fn <- normalizePath(fn)
-
-	ix <- which(DF_BITMAPS$hash == hash)
-
-	if (length(ix) != 1)
-	{
-		DF_WARNINGS <<- rbind(DF_WARNINGS, warn_event(level = 'WARN', source = web_source, issue = paste('Unknown bitmap file', fn)))
-
-		return(FALSE)
-	}
-
-	if (DF_BITMAPS$found[ix] == '') {
-		DF_BITMAPS$found[ix] <<- fn
-	} else {
-		if (DF_BITMAPS$found[ix] != fn)
-		{
-			DF_WARNINGS <<- rbind(DF_WARNINGS, warn_event(level = 'NOTE', source = web_source, issue = paste('Multiple instances of bitmap', fn)))
-
-			return(FALSE)
+	if (length(ix) == 1) {
+		if (GLOBAL$bitmaps$found[ix] == '') GLOBAL$bitmaps$found[ix] <- fn
+		else {
+			if (GLOBAL$bitmaps$found[ix] != fn) warning(level = 'NOTE', source = web_source, issue = paste('Multiple instances of bitmap', nice(fn)))
 		}
-	}
-
-	TRUE
+	} else warning(level = 'WARN', source = web_source, issue = paste('Unknown bitmap file', nice(fn)))
 }
 
 
 audit_js <- function(fn, web_source)
 {
+	fn	 <- normalizePath(fn)
 	hash <- digest::digest(fn, file = TRUE)
 
-	if (nrow(DF_JS) == 0) DF_JS <<- hash_folder(PATH_KNOWN_JS)
+	ix <- which(GLOBAL$jscript$hash == hash)
 
-	fn <- normalizePath(fn)
-
-	ix <- which(DF_JS$hash == hash)
-
-	if (length(ix) != 1)
-	{
-		DF_WARNINGS <<- rbind(DF_WARNINGS, warn_event(level = 'WARN', source = web_source, issue = paste('Unknown js', fn)))
-
-		return(FALSE)
-	}
-
-	if (DF_JS$found[ix] == '') {
-		DF_JS$found[ix] <<- fn
-	} else {
-		if (DF_JS$found[ix] != fn)
-		{
-			DF_WARNINGS <<- rbind(DF_WARNINGS, warn_event(level = 'NOTE', source = web_source, issue = paste('Multiple instances of js', fn)))
-
-			return(FALSE)
+	if (length(ix) == 1) {
+		if (GLOBAL$jscript$found[ix] == '') GLOBAL$jscript$found[ix] <- fn
+		else {
+			if (GLOBAL$jscript$found[ix] != fn) warning(level = 'NOTE', source = web_source, issue = paste('Multiple instances of js', nice(fn)))
 		}
-	}
-
-	TRUE
+	} else warning(level = 'WARN', source = web_source, issue = paste('Unknown js file', nice(fn)))
 }
 
 
 audit_font <- function(fn, web_source)
 {
-	# system(paste('cp', fn, PATH_KNOWN_FONTS))
-	#
-	# return(T)
-
+	fn	 <- normalizePath(fn)
 	hash <- digest::digest(fn, file = TRUE)
 
-	if (nrow(DF_FONTS) == 0) DF_FONTS <<- hash_folder(PATH_KNOWN_FONTS)
+	ix <- which(GLOBAL$fonts$hash == hash)
 
-	fn <- normalizePath(fn)
-
-	ix <- which(DF_FONTS$hash == hash)
-
-	if (length(ix) != 1)
-	{
-		DF_WARNINGS <<- rbind(DF_WARNINGS, warn_event(level = 'WARN', source = web_source, issue = paste('Unknown font file', fn)))
-
-		return(FALSE)
-	}
-
-	if (DF_FONTS$found[ix] == '') {
-		DF_FONTS$found[ix] <<- fn
-	} else {
-		if (DF_FONTS$found[ix] != fn)
-		{
-			DF_WARNINGS <<- rbind(DF_WARNINGS, warn_event(level = 'NOTE', source = web_source, issue = paste('Multiple instances of font', fn)))
-
-			return(FALSE)
+	if (length(ix) == 1) {
+		if (GLOBAL$fonts$found[ix] == '') GLOBAL$fonts$found[ix] <- fn
+		else {
+			if (GLOBAL$fonts$found[ix] != fn) warning(level = 'NOTE', source = web_source, issue = paste('Multiple instances of font', nice(fn)))
 		}
-	}
-
-	TRUE
-}
-
-
-report_crap <- function(fn, web_source)
-{
-	DF_WARNINGS <<- rbind(DF_WARNINGS, warn_event(level = 'WARN', source = web_source, issue = paste('Crap file', normalizePath(fn))))
+	} else warning(level = 'WARN', source = web_source, issue = paste('Unknown font file', nice(fn)))
 }
 
 
@@ -247,7 +225,7 @@ audit_static <- function(fn, ext, web_source)
 	if (ext == 'css')  return(audit_css (fn, web_source))
 	if (ext == 'html') return(audit_html(fn, web_source))
 
-	stop('What is this?')
+	stop('Unsupported extension.')
 }
 
 
@@ -262,13 +240,13 @@ audit_file <- function(fn, web_source)
 	bytes <- ll$size
 	last  <- format(ll$mtime, format = '%Y-%m-%d %H:%M:%S')
 
-	ix <- which(DF_STATS$type == ext)
+	ix <- which(GLOBAL$stats$type == ext)
 
-	if (length(ix) == 0) DF_STATS <<- rbind(DF_STATS, stat_even(type = ext, files = 1, bytes = bytes, last = last))
+	if (length(ix) == 0) GLOBAL$stats <- rbind(GLOBAL$stats, stat_event(type = ext, files = 1, bytes = bytes, last = last))
 	else {
-		DF_STATS$files[ix] <<- DF_STATS$files[ix] + 1
-		DF_STATS$bytes[ix] <<- DF_STATS$bytes[ix] + bytes
-		DF_STATS$last [ix] <<- max(DF_STATS$last[ix], last)
+		GLOBAL$stats$files[ix] <- GLOBAL$stats$files[ix] + 1
+		GLOBAL$stats$bytes[ix] <- GLOBAL$stats$bytes[ix] + bytes
+		GLOBAL$stats$last [ix] <- max(GLOBAL$stats$last[ix], last)
 	}
 
 	if (!(ext %in% KNOWN_EXTENSIONS)) stop(paste('Unknown extension', ext, 'in', fn))
@@ -276,7 +254,7 @@ audit_file <- function(fn, web_source)
 	if (ext %in% BITMAP_EXTENSIONS) audit_bitmap(fn, web_source)
 	if (ext %in% STATIC_EXTENSIONS) audit_static(fn, ext, web_source)
 	if (ext %in% FONTS_EXTENSIONS)	audit_font	(fn, web_source)
-	if (ext %in% CRAP_EXTENSIONS)	report_crap (fn, web_source)
+	if (ext %in% CRAP_EXTENSIONS)	warning (level = 'WARN', source = web_source, issue = paste('Crap file', fn))
 }
 
 
@@ -298,34 +276,35 @@ audit <- function(folders)
 }
 
 
-colnames_toupper <- function(df)
+doit <- function()
 {
-	colnames(df) <- toupper(colnames(df))
-	rownames(df) <- NULL
-	df
+	invisible(sapply(ALL_FOLDERS, audit))
+
+	err <- try(sx <- gsub('^.*columns[[:blank:]]*([0-9]+)[[:blank:]]*;.*$', '\\1', system('stty -a | head -1', intern = T)), silent = TRUE)
+
+	if (class(err) != 'try-error' && length(sx) == 1) options(width = as.integer(sx))
+
+	colnames_toupper <- function(df) {colnames(df) <- toupper(colnames(df)); rownames(df) <- NULL; df}
+
+	cat('\n')
+
+	if (nrow(GLOBAL$warnings) == 0) {
+		cat('No errors, no warnings.\n')
+	} else{
+		ix <- order(GLOBAL$warnings$level, GLOBAL$warnings$source, GLOBAL$warnings$issue)
+		print(colnames_toupper(GLOBAL$warnings))
+	}
+
+	cat('\n')
+
+	if (nrow(GLOBAL$stats) == 0) {
+		cat('No statistics available.\n')
+	} else{
+		ix <- order(GLOBAL$stats$files, GLOBAL$stats$type, decreasing = c(T, F), method = 'radix')
+		print(colnames_toupper(GLOBAL$stats[ix, ]))
+	}
 }
 
 
-invisible(sapply(ALL_FOLDERS, audit))
-
-err <- try(sx <- gsub('^.*columns[[:blank:]]*([0-9]+)[[:blank:]]*;.*$', '\\1', system('stty -a | head -1', intern = T)), silent = TRUE)
-
-if (class(err) != 'try-error' && length(sx) == 1) options(width = as.integer(sx))
-
-cat('\n')
-
-if (nrow(DF_WARNINGS) == 0) {
-	cat('No errors, no warnings.\n')
-} else{
-	ix <- order(DF_WARNINGS$level, DF_WARNINGS$source, DF_WARNINGS$issue)
-	print(colnames_toupper(DF_WARNINGS))
-}
-
-cat('\n')
-
-if (nrow(DF_STATS) == 0) {
-	cat('No statistics available.\n')
-} else{
-	ix <- order(DF_STATS$files, DF_STATS$type, decreasing = c(T, F), method = 'radix')
-	print(colnames_toupper(DF_STATS[ix, ]))
-}
+load_globals()
+doit()

4783a194c1979e161a1e9c4b91e5bbc6fb7c907d
kaalam <kaalam@dybot.com>
2018-02-05 19:37:34 +0100

    Self explanatory configuration file

--- audit.conf.r
+++ audit.conf.r
@@ -1,3 +1,16 @@
-SKIP_WEB_SOURCES <- c('_blog_', '_doc_', '_doxy1_', '_doxy2_', '_kaal_', '_news_', '_pycli_', '_rcli_', '_stat_')
+# SKIP_WEB_SOURCES <- c('_blog_', '_doc_', '_doxy1_', '_doxy2_', '_kaal_', '_news_', '_pycli_', '_rcli_', '_stat_')
 
-SKIP_CRAP_CHECK <- T
+# SKIP_BLACKLISTED_HTML <- T
+# SKIP_CRAP_CHECK       <- T
+
+# SKIP_EXTRACT_HTTP_CSS  <- T
+# SKIP_EXTRACT_HTTP_HTML <- T
+# SKIP_EXTRACT_URL_CSS   <- T
+
+# SKIP_KNOWN_BITMAPS <- T
+# SKIP_KNOWN_FONTS   <- T
+# SKIP_KNOWN_JS      <- T
+
+# SKIP_MULTIPLE_BITMAPS <- T
+# SKIP_MULTIPLE_FONTS   <- T
+# SKIP_MULTIPLE_JS      <- T

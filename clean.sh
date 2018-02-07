eadf03b71746c98dc02b25cc30d562ceb1db35d2
kaalam <kaalam@dybot.com>
2018-02-07 22:04:05 +0100

    Cleaning utility to clean instead of ignoring.

---
+++ clean.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+rm jekyll_documentation/_config.yml
+rm -rf jekyll_documentation/_posts/
+rm -rf jekyll_documentation/_data/
+rm -rf jekyll_documentation/images/
+rm -rf jekyll_documentation/index.md
+rm -rf jekyll_documentation/pages/
+
+rm -rf jekyll_evento/.sass-cache/
+rm jekyll_evento/_config.yml
+rm -rf jekyll_evento/_data/
+rm -rf jekyll_evento/_posts/
+rm -rf jekyll_evento/about.md
+
+rm -rf jekyll_forty/.sass-cache/
+rm jekyll_forty/Gemfile.lock
+rm -rf jekyll_forty/_posts/
+rm -rf jekyll_forty/_config.yml
+rm -rf jekyll_forty/index.md
+rm -rf jekyll_forty/assets/images/
+
+rm -rf jekyll_so-simple/.sass-cache/
+rm jekyll_so-simple/Gemfile.lock
+rm -rf jekyll_so-simple/_data/
+rm -rf jekyll_so-simple/_posts/
+rm -rf jekyll_so-simple/about/
+rm -rf jekyll_so-simple/articles/
+rm jekyll_so-simple/_config.yml
+rm -rf jekyll_so-simple/blog/
+rm -rf jekyll_so-simple/images/
+rm -rf jekyll_so-simple/search/
+rm -rf jekyll_so-simple/tags/
+rm -rf jekyll_so-simple/theme-setup/

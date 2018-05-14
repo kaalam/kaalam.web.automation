baaad39e218d767662dadee09fa2d5e4d9414cbf
kaalam <kaalam@dybot.com>
2018-05-14 18:52:36 +0200

    Updates audit config to focus on the urls after refactoring the site

--- audit.conf.r
+++ audit.conf.r
@@ -1,4 +1,5 @@
 # SKIP_WEB_SOURCES <- c('_blog_', '_doc_', '_doxy1_', '_doxy2_', '_kaal_', '_news_', '_pycli_', '_rcli_', '_stat_')
+SKIP_WEB_SOURCES <- c('_rcli_')
 
 # SKIP_BLACKLISTED_HTML <- T
 # SKIP_CRAP_CHECK       <- T
@@ -7,9 +8,9 @@
 # SKIP_EXTRACT_HTTP_HTML <- T
 # SKIP_EXTRACT_URL_CSS   <- T
 
-# SKIP_KNOWN_BITMAPS <- T
-# SKIP_KNOWN_FONTS   <- T
-# SKIP_KNOWN_JS      <- T
+SKIP_KNOWN_BITMAPS <- T
+SKIP_KNOWN_FONTS   <- T
+SKIP_KNOWN_JS      <- T
 
 SKIP_MULTIPLE_BITMAPS <- T
 SKIP_MULTIPLE_FONTS   <- T

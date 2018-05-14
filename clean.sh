c0fa8c070b44ee68dbc9fa6c4c3a356ee8f3cc0c
kaalam <kaalam@dybot.com>
2018-05-14 17:14:17 +0200

    Marks folders related with news and blog as remarks

--- clean.sh
+++ clean.sh
@@ -7,11 +7,11 @@ rm -rf jekyll_documentation/images/
 rm -rf jekyll_documentation/index.md
 rm -rf jekyll_documentation/pages/
 
-rm -rf jekyll_evento/.sass-cache/
-rm jekyll_evento/_config.yml
-rm -rf jekyll_evento/_data/
-rm -rf jekyll_evento/_posts/
-rm -rf jekyll_evento/about.md
+# rm -rf jekyll_evento/.sass-cache/
+# rm jekyll_evento/_config.yml
+# rm -rf jekyll_evento/_data/
+# rm -rf jekyll_evento/_posts/
+# rm -rf jekyll_evento/about.md
 
 rm -rf jekyll_forty/.sass-cache/
 rm jekyll_forty/Gemfile.lock
@@ -20,15 +20,15 @@ rm -rf jekyll_forty/_config.yml
 rm -rf jekyll_forty/index.md
 rm -rf jekyll_forty/assets/images/
 
-rm -rf jekyll_so-simple/.sass-cache/
-rm jekyll_so-simple/Gemfile.lock
-rm -rf jekyll_so-simple/_data/
-rm -rf jekyll_so-simple/_posts/
-rm -rf jekyll_so-simple/about/
-rm -rf jekyll_so-simple/articles/
-rm jekyll_so-simple/_config.yml
-rm -rf jekyll_so-simple/blog/
-rm -rf jekyll_so-simple/images/
-rm -rf jekyll_so-simple/search/
-rm -rf jekyll_so-simple/tags/
-rm -rf jekyll_so-simple/theme-setup/
+# rm -rf jekyll_so-simple/.sass-cache/
+# rm jekyll_so-simple/Gemfile.lock
+# rm -rf jekyll_so-simple/_data/
+# rm -rf jekyll_so-simple/_posts/
+# rm -rf jekyll_so-simple/about/
+# rm -rf jekyll_so-simple/articles/
+# rm jekyll_so-simple/_config.yml
+# rm -rf jekyll_so-simple/blog/
+# rm -rf jekyll_so-simple/images/
+# rm -rf jekyll_so-simple/search/
+# rm -rf jekyll_so-simple/tags/
+# rm -rf jekyll_so-simple/theme-setup/

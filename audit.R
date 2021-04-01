afc0f3d7b8393b0b42258e329718dff4054c766e
kaalam <kaalam@kaalam.ai>
2021-04-01 16:29:08 +0200

    Avoids extract_url triggerinf the double check

--- audit.R
+++ audit.R
@@ -24,7 +24,7 @@ hashed_folder <- function(hash, name, type, found) data.frame(hash = hash, name 
 warn_event	  <- function(level, source, issue) data.frame(level = level, source = source, issue = issue, stringsAsFactors = FALSE)
 stat_event	  <- function(type, files, bytes, last) data.frame(type = type, files = files, bytes = bytes, last = last, stringsAsFactors = FALSE)
 
-extract_url	 <- list(signal = '\\<url\\>', neat = 'url', capture = '^.*\\<url\\>\\(([^)]*)\\).*$', except = '^url"')
+extract_url	 <- list(signal = '\\<url\\>', neat = 'url', capture = '^.*\\<url\\>\\(([^)]*)\\).*$', except = '.*')
 extract_http <- list(signal = '\\<https?://', neat = 'http://', capture = '^.*\\<https?://([[:alnum:]_/.-]+).*$', except = '^https?://"')
 
 GLOBAL <- new.env()
@@ -131,7 +131,7 @@ extract_capture <- function(txt, capture, fn, web_source)
 
 	ix <- which(!grepl(capture$capture, txt))
 
-	for (tx in txt[ix]){
+	for (tx in txt[ix]) {
 		if (!grepl(capture$except, nice(tx)))
 			warning (level = 'WARN', source = web_source, issue = paste('Cannot capture in', nice(tx), 'in', nice(fn)))
 	}

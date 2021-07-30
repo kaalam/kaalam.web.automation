115eaf67da34e507a18d80f715fe092c134b0375
kaalam <kaalam@kaalam.ai>
2021-07-30 22:38:26 +0200

    Automates pushing

--- render_release.R
+++ render_release.R
@@ -4,9 +4,6 @@ source('automation_utils.R')
 
 build_all()
 
-writeLines('<html><head><meta http-equiv="refresh" content="0; url=https://kaalam.github.io/kaalam/index.html"></head></html>',
-		   '../kaalam.github.io/index.html')
+writeLines('<html><head><meta http-equiv="refresh" content="0; url=https://kaalam.github.io/kaalam/index.html"></head></html>', '../kaalam.github.io/index.html')
 
-setwd('../kaalam.github.io/')
-
-system('./auto_add_commit_push.sh')
+system('./audit.R && cd ../kaalam.github.io/ && ./auto_add_commit_push.sh')

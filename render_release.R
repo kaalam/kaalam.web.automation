a7c2f8b4903b2442f4b9109e6fdf9dc63c7664f3
kaalam <kaalam@kaalam.ai>
2021-08-01 13:53:40 +0200

    Removes duplicates for known_js to automate the whole thing

--- render_release.R
+++ render_release.R
@@ -6,4 +6,6 @@ build_all()
 
 writeLines('<html><head><meta http-equiv="refresh" content="0; url=https://kaalam.github.io/kaalam/index.html"></head></html>', '../kaalam.github.io/index.html')
 
+system('cd ~/kaalam.etc/web_security/known_js/ && cd ~/kaalam.etc/web_security/known_js/')
+
 system('./audit.R && cd ../kaalam.github.io/ && ./auto_add_commit_push.sh')
